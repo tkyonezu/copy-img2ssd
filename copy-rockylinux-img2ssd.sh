28446253f7d198446d7977d6f5dbd510fed44e67
Takeshi Yonezu <tkyonezu@gmail.com>
2023-09-04 22:33:22 +0900

    Refactoring

--- copy-rockylinux-img2ssd.sh
+++ copy-rockylinux-img2ssd.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+SSDDEV=/dev/sda
+BOOTSZ=512M
+
+ROCKY_URL="https://dl.rockylinux.org/pub/sig/9/altarch/aarch64/images/"
+ROCKY_IMG="RockyLinuxRpi_9-latest.img"
+
 logmsg() {
   echo ">>> $1"
 }
@@ -23,36 +29,36 @@ function get_uuid() {
 logmsg "Start Copy Rocky Linux to SSD"
 
 logmsg "Create partitions"
-sudo parted /dev/sda mklabel msdos
-sudo parted /dev/sda mkpart primary fat32 0% 512M   
-sudo parted /dev/sda mkpart primary ext4 512M 100%
+sudo parted ${SSDDEV} mklabel msdos
+sudo parted ${SSDDEV} mkpart primary fat32 0% ${BOOTSZ}   
+sudo parted ${SSDDEV} mkpart primary ext4 ${BOOTSZ} 100%
 
-sudo parted /dev/sda print
+sudo parted ${SSDDEV} print
 
 logmsg "Make filesystems"
-sudo mkfs.vfat -F 32 /dev/sda1
-sudo mkfs.ext4 /dev/sda2
+sudo mkfs.vfat -F 32 ${SSDDEV}1
+sudo mkfs.ext4 ${SSDDEV}2
 
-sudo e2label /dev/sda2 "_/"
+sudo e2label ${SSDDEV}2 "_/"
 
 cd /var/tmp
 
 if [ ! -f RockyLinuxRpi_9-latest.img ]; then
   logmsg "Get Latest Rocky Linux image"
-  wget https://dl.rockylinux.org/pub/sig/9/altarch/aarch64/images/RockyLinuxRpi_9-latest.img.xz
+  wget ${ROCKY_URL}${ROCKY_IMG}.xz
 
-  xz -dv RockyLinuxRpi_9-latest.img.xz
+  xz -dv ${ROCKY_IMG}.xz
 fi
 
 logmsg "Loopback mount Rocky Linux image"
-sudo kpartx -av RockyLinuxRpi_9-latest.img
+sudo kpartx -av ${ROCKY_IMG}
 
 logmsg "Copy boot filesystem to SSD"
 sudo rm -fr /var/tmp/mnt2
 sudo mkdir /var/tmp/mnt2
 
 sudo mount /dev/mapper/loop0p1 /mnt
-sudo mount /dev/sda1 /var/tmp/mnt2
+sudo mount ${SSDDEV}1 /var/tmp/mnt2
 
 sudo rsync -avhP /mnt/ /var/tmp/mnt2/
 
@@ -60,28 +66,28 @@ sudo umount /var/tmp/mnt2 /mnt
 
 logmsg "Copy root filesystem to SSD"
 sudo mount /dev/mapper/loop0p3 /mnt
-sudo mount /dev/sda2 /var/tmp/mnt2
+sudo mount ${SSDDEV}2 /var/tmp/mnt2
 
 sudo rsync -avhP --exclude /mnt/lost+found /mnt/ /var/tmp/mnt2/
 
 logmsg "Unmount Rocky Linux image"
 sudo umount /var/tmp/mnt2 /mnt
 
-sudo kpartx -d RockyLinuxRpi_9-latest.img
+sudo kpartx -d ${ROCKY_IMG}
 
 logmsg "Modify PARTUUID of 'cmdline.txt'"
-BPU=$(get_partuuid /dev/sda1)
+BPU=$(get_partuuid ${SSDDEV}1)
 
-sudo mount /dev/sda1 /mnt
+sudo mount ${SSDDEV}1 /mnt
 sudo sed -i "s/PARTUUID=.*-03/PARTUUID=${BPU}-02/" /mnt/cmdline.txt
 sudo sed -i 's/$/ selinux=0/' /mnt/cmdline.txt
 sudo umount /mnt
 
 logmsg "Modify UUID of '/etc/fstab'"
-BUU=$(get_uuid /dev/sda1)
-RUU=$(get_uuid /dev/sda2)
+BUU=$(get_uuid ${SSDDEV}1)
+RUU=$(get_uuid ${SSDDEV}2)
 
-sudo mount /dev/sda2 /mnt
+sudo mount ${SSDDEV}2 /mnt
 
 sudo sed -i "/ ext4 /s/^UUID=.................................... /UUID=${RUU} /" /mnt/etc/fstab
 sudo sed -i "/fat /s/^UUID=......... /UUID=${BUU} /" /mnt/etc/fstab
